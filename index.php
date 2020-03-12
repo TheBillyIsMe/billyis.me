a0b33516f95d9a803ca4b524950cd912dd757d95
Billy Meis <54307189+TheBillyIsMe@users.noreply.github.com>
2020-03-11 23:09:51 -0400

    adds animations and starts projects page
    Signed-off-by: Billy Meis <54307189+TheBillyIsMe@users.noreply.github.com>

--- index.php
+++ index.php
@@ -8,8 +8,8 @@ include('includes/header.php');
 	<div id="particles-js"></div>
 	<h1 class="uk-width-1-2@m uk-text-center uk-margin-auto uk-margin-auto-top uk-margin-medium-bottom">BillyIsMe</h1>
 	<div class="bim-buttons uk-width-1-2@m uk-text-center uk-margin-auto uk-margin-auto-bottom">
-		<a class="uk-button uk-button-default" href="/projects">Projects</a>
-		<a class="uk-button uk-button-default" href="/contact">Contact</a>
+		<a class="bim-projects uk-button uk-button-default" href="/projects">Projects</a>
+		<a class="bim-contact uk-button uk-button-default" href="/contact">Contact</a>
 	</div>
 </div>
 <?php include('includes/footer.php'); ?>
--- projects/index.php
+++ projects/index.php
@@ -1,1 +1,15 @@
-<?php header('Location: /'); ?>
+<?php
+$pageName = 'Projects';
+$pageLevel = 1;
+include('../includes/recursivity.php');
+include('../includes/header.php');
+?>
+<div class="projects-header">
+	<img class="uk-border-circle" width="10%" height="10%" src="../assets/img/logo.png">
+	<div>
+		<h1 class="uk-heading-small">Projects</h1>
+		<hr>
+		<p>Projects I work on currently or in the past.</p>
+	</div>
+</div>
+<?php include('../includes/footer.php'); ?>
